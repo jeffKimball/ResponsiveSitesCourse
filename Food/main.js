2125ad11d3540503713d3d0e5442a4434b558e2b
jKimball <judonomi@gmail.com>
2023-12-04 22:46:31 -0600

    target icons and setup setInterval function to add/remove the change class

---
+++ Food/main.js
@@ -0,0 +1,19 @@
+const icons = document.querySelectorAll('.section-1-icons i')
+let iconCounter = 1
+
+setInterval(()=>{
+    iconCounter++
+    const icon = document.querySelector('.section-1-icons .change')
+
+    icon.classList.remove('change')
+
+    if(iconCounter > icons.length){
+        icons[0].classList.add('change')
+        iconCounter = 1
+    }else {
+        icon.nextElementSibling.classList.add('change')
+    }
+
+    
+
+}, 4000)
